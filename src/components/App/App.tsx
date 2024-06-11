073a00c7e9056bae423cb13018e054ccb2381973
sahshobeyri <sahshobeyri@gmail.com>
2024-06-11 20:40:04 +0330

    lesson title and btns

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -18,11 +18,6 @@ export const App: React.FC<AppProps> = ({ className }) => {
   return (
     <div className={clsx(styles.App, className)}>
       <div className={styles.container}>
-        <h1>یه هدر تستی دیگه</h1>
-        <h2>یه هدر تستی دیگه</h2>
-        <h3>یه هدر تستی دیگه</h3>
-        <h4>یه هدر تستی دیگه</h4>
-        <h5>یه هدر تستی دیگه</h5>
         <a
             href="https://github.com/usernein/react-mini-app"
             target="_blank"
@@ -34,6 +29,15 @@ export const App: React.FC<AppProps> = ({ className }) => {
         <button dir="rtl" onClick={showPopupOnClick} className={styles.clickMe}>
           روی من کلیک کن.
         </button>
+
+        <h1>درس ها</h1>
+        <button dir="rtl" onClick={showPopupOnClick} className={styles.clickMe}>
+          ⭐️ عادت های اتمی
+        </button>
+        <button dir="rtl" onClick={showPopupOnClick} className={styles.clickMe}>
+          ⭐️ تفکر سیستمی
+        </button>
+
       </div>
     </div>
   );
