6ceebd4e293729301bfa8e0766bfaeb3e9e3cc6b
sahshobeyri <sahshobeyri@gmail.com>
2024-06-22 07:28:18 +0330

    card and card frame

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,21 +11,32 @@ export const App: React.FC<AppProps> = ({ className }) => {
 
   return (
     <div className={clsx(styles.App, className)}>
-      <div className={styles.container}>
-          <div dir="rtl" className={styles.header}>
-              <img className={styles.headerLogo} src={logoIcon} alt={"لوگو"}></img>
-              <div className={styles.headerText}>خوشه</div>
-              {/*<img className={styles.headerProfile} src={profileIcon} alt={"پروفایل"}></img>*/}
-          </div>
-          <div dir="rtl" className={styles.bannerFrame}>
-              <div className={styles.banner}>بنر 1</div>
-              <div className={styles.banner}>بنر 2</div>
-              <div className={styles.banner}>بنر 3</div>
-          </div>
-      </div>
+        <div className={styles.container}>
+            <div dir="rtl" className={styles.header}>
+                <img className={styles.headerLogo} src={logoIcon} alt={"لوگو"}></img>
+                <div className={styles.headerText}>خوشه</div>
+            </div>
+            <div dir="rtl" className={styles.bannerFrame}>
+                <div className={styles.banner}>بنر 1</div>
+                <div className={styles.banner}>بنر 2</div>
+                <div className={styles.banner}>بنر 3</div>
+            </div>
+            <div dir="rtl" className={styles.cardFrame}>
+                <div dir="rtl" className={styles.cardFrameTitle}>
+                    پیشنهادی خوشه
+                </div>
+                <div dir="rtl" className={styles.cardSlider}>
+                    <div className={styles.card}>درس 1</div>
+                    <div className={styles.card}>درس 2</div>
+                    <div className={styles.card}>درس 3</div>
+                </div>
+            </div>
+        </div>
+
+        {/*<img className={styles.headerProfile} src={profileIcon} alt={"پروفایل"}></img>*/}
         {/*<div className={styles.container}>*/}
-      {/*  <a*/}
-      {/*      href="https://github.com/usernein/react-mini-app"*/}
+        {/*  <a*/}
+        {/*      href="https://github.com/usernein/react-mini-app"*/}
       {/*      target="_blank"*/}
       {/*      className={styles.githubLink}*/}
       {/*  >*/}
