4be50b5bcd9cf0f6d359d816c9da4e5bf649c938
sahshobeyri <sahshobeyri@gmail.com>
2024-06-22 05:48:39 +0330

    header correction

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./App.module.scss";
 import clsx from "clsx";
-import logoIcon from "./kicon.png"
+import logoIcon from "./kicon2.png"
 import profileIcon from "./profile.png"
 
 export type AppProps = {
@@ -15,7 +15,7 @@ export const App: React.FC<AppProps> = ({ className }) => {
           <div dir="rtl" className={styles.header}>
               <img className={styles.headerLogo} src={logoIcon} alt={"لوگو"}></img>
               <div className={styles.headerText}>خوشه</div>
-              <img className={styles.headerProfile} src={profileIcon} alt={"پروفایل"}></img>
+              {/*<img className={styles.headerProfile} src={profileIcon} alt={"پروفایل"}></img>*/}
           </div>
       </div>
         {/*<div className={styles.container}>*/}
