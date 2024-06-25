087edb48b91d8dffcc4b6ea77c28961873247738
sahshobeyri <sahshobeyri@gmail.com>
2024-06-25 18:44:51 +0330

    add bookmark btn to footer

--- src/components/MainFooter/MainFooter.tsx
+++ src/components/MainFooter/MainFooter.tsx
@@ -3,6 +3,7 @@ import styles from "./MainFooter.module.scss";
 import {NavLink} from "react-router-dom";
 import ProfileIcon from "./FigmaProfile.png"
 import HomeIcon from "./explore.png"
+import BookmarkIcon from "./bookmark.png"
 
 export type MainFooterProps = {
     className?: string;
@@ -11,7 +12,6 @@ export type MainFooterProps = {
 export const MainFooter: React.FC<MainFooterProps> = () => {
     return (
         <div dir="rtl" className={styles.footer}>
-            <div dir="rtl" className={styles.footerText}>سلام سلام</div>
             <div dir="rtl" className={styles.footerProfile}>
                 <NavLink to="/profile">
                     <img src={ProfileIcon} alt="profile-img"/>
@@ -19,7 +19,12 @@ export const MainFooter: React.FC<MainFooterProps> = () => {
             </div>
             <div dir="rtl" className={styles.footerHome}>
                 <NavLink to="/">
-                    <img src={HomeIcon} alt="explore-img"/>
+                    <img src={HomeIcon} alt="home-img"/>
+                </NavLink>
+            </div>
+            <div dir="rtl" className={styles.footerHome}>
+                <NavLink to="/">
+                    <img src={BookmarkIcon} alt="bookmark-img"/>
                 </NavLink>
             </div>
         </div>
