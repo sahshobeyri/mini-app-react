45e677d593d1eeebc2039cc88adcf5af70c7b9fd
sahshobeyri <sahshobeyri@gmail.com>
2024-06-25 22:07:18 +0330

    SuggestUs.tsx back to home NavLink

--- src/components/pages/SuggestUs/SuggestUs.tsx
+++ src/components/pages/SuggestUs/SuggestUs.tsx
@@ -3,6 +3,7 @@ import styles from "./SuggestUs.module.scss";
 import clsx from "clsx";
 import {MainFooter} from "../../MainFooter/MainFooter.tsx"
 import BackIcon from "./BackBtn2.png"
+import {NavLink} from "react-router-dom";
 
 export type SuggestUsProps = {
   className?: string;
@@ -13,7 +14,9 @@ export const SuggestUs: React.FC<SuggestUsProps> = ({ className }) => {
     <div className={clsx(styles.SuggestUs, className)}>
         <div className={styles.container}>
             <div dir="rtl" className={styles.header}>
-                <img className={styles.headerBackBtn} src={BackIcon} alt="back-btn"></img>
+                <NavLink to="/">
+                    <img className={styles.headerBackBtn} src={BackIcon} alt="back-btn"></img>
+                </NavLink>
                 <div className={styles.headerText}>پیشنهاد به خوشه</div>
             </div>
 
