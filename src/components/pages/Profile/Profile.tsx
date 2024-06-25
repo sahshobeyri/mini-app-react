7f7c99a3573fbb1c50aa1694607cf6883a01186b
sahshobeyri <sahshobeyri@gmail.com>
2024-06-25 17:01:55 +0330

    link back to home in profile

--- src/components/pages/Profile/Profile.tsx
+++ src/components/pages/Profile/Profile.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./Profile.module.scss";
 import clsx from "clsx";
+import logoIcon from "../Home/kicon2.png";
+import {NavLink} from "react-router-dom";
+import profileIcon from "../Home/FigmaProfile.png";
 
 export type ProfileProps = {
   className?: string;
@@ -9,7 +12,22 @@ export const Profile: React.FC<ProfileProps> = ({ className }) => {
 
   return (
     <div className={clsx(styles.Profile, className)}>
-        <div> سلام سلام سلام</div>
+        <div className={styles.container}>
+            <div dir="rtl" className={styles.header}>
+                <img className={styles.headerLogo} src={logoIcon} alt={"لوگو"}></img>
+                <div className={styles.headerText}>خوشه</div>
+            </div>
+
+            <div dir="rtl" className={styles.footer}>
+                <div dir="rtl" className={styles.title}>اینجا فوتر است</div>
+                <div dir="rtl" className={styles.footerProfile}>
+                    <NavLink to="/">
+                        <img src={profileIcon} alt="profile-img"/>
+                    </NavLink>
+                </div>
+            </div>
+
+        </div>
     </div>
   );
 };
