94594fd21e0a7c953b68fb96f7590b9fe3e22e65
sahshobeyri <sahshobeyri@gmail.com>
2024-06-25 22:05:40 +0330

    violet header

--- src/components/pages/SuggestUs/SuggestUs.tsx
+++ src/components/pages/SuggestUs/SuggestUs.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./SuggestUs.module.scss";
 import clsx from "clsx";
 import {MainFooter} from "../../MainFooter/MainFooter.tsx"
+import BackIcon from "./BackBtn2.png"
 
 export type SuggestUsProps = {
   className?: string;
@@ -12,6 +13,7 @@ export const SuggestUs: React.FC<SuggestUsProps> = ({ className }) => {
     <div className={clsx(styles.SuggestUs, className)}>
         <div className={styles.container}>
             <div dir="rtl" className={styles.header}>
+                <img className={styles.headerBackBtn} src={BackIcon} alt="back-btn"></img>
                 <div className={styles.headerText}>پیشنهاد به خوشه</div>
             </div>
 
